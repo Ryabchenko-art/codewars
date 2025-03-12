f70fca15677c5af27af59e93115d699b31376168
Ryabchenko-art <erabcenko4@gmail.com>
2025-03-12 12:54:33 +0300

    solution

--- 8kyu/grasshopper_terminal_game_combat.js
+++ 8kyu/grasshopper_terminal_game_combat.js
@@ -1,11 +1,18 @@
 /*Create a combat function that takes the player's current health and
 the amount of damage received, and returns the player's new health.
 Health can't be less than 0.
-*/
+ */
+
 function combat(health, damage) {
-
-    return ((health - damage) && health > 0)
+    let newHealth = health - damage
+    health = health - newHealth
+    if (newHealth >= 0) {
+        return newHealth
+    }
+    else if (newHealth < 0){
+        return newHealth = 0;
+    }
 }
-
-console.log(combat(100, 101))
+console.log(combat(20, 30))
 // Write your code here//
+
